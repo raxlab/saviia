7172d9e0a3c13d10d10916e3a16ef4c8a61254a1
PEDRO ZAVALA <pedropablozavalat@uc.cl>
2025-04-13 02:18:17 -0400

    fix: remove unecessary code and edit requirements.txt

--- custom_components/rcer_datahub/__init__.py
+++ custom_components/rcer_datahub/__init__.py
@@ -4,27 +4,14 @@ import logging
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, ServiceCall
+from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN
-from .libs.ftp_client import FTPClient
 
 _LOGGER = logging.getLogger(__name__)
 
-
-async def async_setup(hass: HomeAssistant, entry: ConfigEntry):
-    async def handle_ftp_list_files(call: ServiceCall):
-        host = call.data.get("host")
-        user = call.data.get("user")
-        password = call.data.get("password")
-        path = call.data.get("path", "/")
-
-        try:
-            ftp = FTPClient(host, user, password, path)
-            files = ftp.list_files()
-            _LOGGER.info(f"Files: {files}")
-        except Exception as e:
-            _LOGGER.error(f"FTP error: {e}")
-
-    hass.services.async_register(DOMAIN, "ftp_list_files", handle_ftp_list_files)
-
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool: 
     return True
+
+async def async_setup_entry(hass: HomeAssistant, config: ConfigType) -> bool:
+    pass
