18569e116c71faab3f8537f64b77483e41163d2e
PEDRO ZAVALA <pedropablozavalat@uc.cl>
2025-04-14 03:26:52 -0400

    refactor: change file names

--- custom_components/rcer_datahub/coordinator.py
+++ custom_components/rcer_datahub/coordinator.py
@@ -1,17 +1,15 @@
-import os
 from datetime import timedelta
 
 from dotenv import load_dotenv
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
-from ...libs.http_client import HTTPClient, HttpClientInitArgs
 from .const import LOGGER, UPDATE_INTERVAL_HOURS, UPDATE_INTERVAL_MINUTES
 
 load_dotenv()
 
 
-class RCERDataHubCoordinator(DataUpdateCoordinator):
+class RCERDatahubCoordinator(DataUpdateCoordinator):
     """Class to manage remote extraction workflow from EPII"""
 
     def __init__(self, hass: HomeAssistant):
@@ -22,13 +20,6 @@ class RCERDataHubCoordinator(DataUpdateCoordinator):
             update_interval=timedelta(
                 hours=UPDATE_INTERVAL_HOURS, minutes=UPDATE_INTERVAL_MINUTES
             ),
-        )
-        self.http_client = HTTPClient(
-            HttpClientInitArgs(
-                client_name="request_client",
-                access_token=os.getenv("MICROSOFT_GRAPH_ACCESS_TOKEN"),
-                base_url="https://graph.microsoft.com/v1.0/"
-            )
         )
 
     async def _async_setup(self) -> None:
