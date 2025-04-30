b830b2ee822b83a0ca8da952ce58d4707002cbd7
PEDRO ZAVALA <pedropablozavalat@uc.cl>
2025-04-30 15:42:12 -0400

    refactor: clean code and update class names

--- custom_components/rcer_datahub/config_flow.py
+++ custom_components/rcer_datahub/config_flow.py
@@ -7,16 +7,17 @@ from typing import TYPE_CHECKING, Any
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.data_entry_flow import FlowResult
-from rcer_iot_client_pkg import EpiiUpdateThiesConfig
 
-from .const import DOMAIN, LOGGER
+from .const import DOMAIN
 
 if TYPE_CHECKING:
     from homeassistant.data_entry_flow import FlowResult
 
 
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Config flow for RCER Data Hub."""
+class SyncThiesDataConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Config flow for Thies Data Logger."""
+
+    VERSION = 1
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -25,24 +26,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
         if user_input is not None:
             try:
-                EpiiUpdateThiesConfig(
-                    ftp_host=user_input["ftp_host"],
-                    ftp_port=user_input["ftp_port"],
-                    ftp_user=user_input["ftp_user"],
-                    ftp_password=user_input["ftp_password"],
-                    sharepoint_client_id=user_input["sharepoint_client_id"],
-                    sharepoint_client_secret=user_input["sharepoint_client_secret"],
-                    sharepoint_tenant_id=user_input["sharepoint_tenant_id"],
-                    sharepoint_tenant_name=user_input["sharepoint_tenant_name"],
-                    sharepoint_site_name=user_input["sharepoint_site_name"],
-                )
                 return self.async_create_entry(title="RCER Data Hub", data=user_input)
             except ValueError:
                 errors["base"] = "invalid_input"
-            except Exception as e:
-                LOGGER.exception(f"Unexpected exception: {e}")
-                errors["base"] = "unknown"
-                raise
         data_schema = vol.Schema(
             {
                 vol.Required("ftp_host"): str,
--- custom_components/rcer_datahub/coordinator.py
+++ custom_components/rcer_datahub/coordinator.py
@@ -1,41 +1,63 @@
+from datetime import timedelta
+
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
-from rcer_iot_client_pkg import EpiiAPI
+from rcer_iot_client_pkg import EpiiAPI, EpiiUpdateThiesConfig
 
 from custom_components.rcer_datahub.helpers.datetime_utils import datetime_to_str, today
 
-from .const import EPII_API_CONFIG
+from .const import LOGGER, UPDATE_INTERVAL_HOURS, UPDATE_INTERVAL_MINUTES
 
 
-class RCERDatahubUpdateCoordinator(DataUpdateCoordinator):
+class SyncThiesDataCoordinator(DataUpdateCoordinator):
     """Class to manage remote extraction workflow at EPII."""
 
     def __init__(
-        self, hass: HomeAssistant, api: EpiiAPI, logger, name, update_interval
+        self,
+        hass: HomeAssistant,
+        config_entry: ConfigEntry,
+        api: EpiiAPI,
     ) -> None:
         """Set up the coordinator."""
-        self.api = api
-        self.last_update = None
-        self.data: dict[str, dict] = {}
-        super().__init(
+        super().__init__(
             hass,
-            logger,
-            name,
-            update_interval,
+            LOGGER,
+            config_entry=config_entry,
+            update_interval=timedelta(
+                hours=UPDATE_INTERVAL_HOURS,
+                minutes=UPDATE_INTERVAL_MINUTES,
+            ),
             always_update=True,
         )
+        self.api = api
+        self.config_entry = config_entry
+        self.update_thies_config = EpiiUpdateThiesConfig(
+            ftp_port=config_entry.data["ftp_port"],
+            ftp_host=config_entry.data["ftp_host"],
+            ftp_user=config_entry.data["ftp_user"],
+            ftp_password=config_entry.data["ftp_password"],
+            sharepoint_client_id=config_entry.data["sharepoint_client_id"],
+            sharepoint_client_secret=config_entry.data["sharepoint_client_secret"],
+            sharepoint_tenant_id=config_entry.data["sharepoint_tenant_id"],
+            sharepoint_tenant_name=config_entry.data["sharepoint_tenant_name"],
+            sharepoint_site_name=config_entry.data["sharepoint_site_name"],
+        )
+        self.last_update: str | None = None
+        self.data: dict[str, dict] = {}
 
     async def _async_update_data(self) -> dict:
         """Upload data from RCER API and get response."""
+        self.logger.debug("[coordinator] async_update_data_started")
         try:
-            self.logger.debug("[coordinator] async_update_data_started")
-            synced_files = await self.api.update_thies_data(EPII_API_CONFIG)
+            synced_files = await self.api.update_thies_data(self.update_thies_config)
             self.data = synced_files
             self.last_update = datetime_to_str(today())
             self.logger.debug(
-                "[coordinator] async_update_data_successful", extra=synced_files
+                "[coordinator] async_update_data_successful",
+                extra={"synced_files": synced_files},
             )
-            return synced_files
+            return {"synced_files": synced_files}
         except Exception as e:
             self.logger.error(
                 "[coordinator] async_update_data_error", extra={"error": e.__str__()}
