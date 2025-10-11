d66fe23865771d2f9e659f7c6e674c0e8f6f4385
PEDRO ZAVALA <pedropablozavalat@uc.cl>
2025-10-10 23:39:47 -0300

    bump: replace EPII API with Saviia API

--- custom_components/saviia/coordinator.py
+++ custom_components/saviia/coordinator.py
@@ -3,7 +3,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import (
     DataUpdateCoordinator,
 )
-from saviialib import EpiiAPI
+from saviialib import SaviiaAPI
 
 from custom_components.saviia.helpers.datetime_utils import datetime_to_str, today
 
@@ -15,7 +15,7 @@ class SaviiaBaseCoordinator(DataUpdateCoordinator):
         self,
         hass: HomeAssistant,
         config_entry: ConfigEntry,
-        api: EpiiAPI,
+        api: SaviiaAPI,
     ) -> None:
         """Set up the coordinator."""
         super().__init__(
@@ -45,12 +45,13 @@ class SyncThiesDataCoordinator(SaviiaBaseCoordinator):
             config_entry.data["thies_ftp_server_avg_path"],
             config_entry.data["thies_ftp_server_ext_path"],
         ]
+        self.thies_service = api.get('thies')
 
     async def _async_update_data(self) -> dict:
         """Upload data using the Epii API from SAVIIA library and get uploaded files."""
         self.logger.info("[%s] async_update_data_started", self.name)
         try:
-            synced_files = await self.api.update_thies_data(
+            synced_files = await self.thies_service.update_thies_data(
                 sharepoint_folders_path=self.sharepoint_folders_path,
                 ftp_server_folders_path=self.ftp_server_folders_path,
             )
@@ -79,12 +80,13 @@ class LocalBackupCoordinator(SaviiaBaseCoordinator):
         self.sharepoint_backup_base_url = config_entry.data[
             "sharepoint_backup_base_url"
         ]
+        self.backup_service = api.get('backup')
 
     async def _async_update_data(self) -> dict:
         """Execute the local backup, extracting files from the source path requested."""
         self.logger.info("[%s] async_local_backup_started", self.name)
         try:
-            exported_files = await self.api.upload_backup_to_sharepoint(
+            exported_files = await self.backup_service.upload_backup_to_sharepoint(
                 local_backup_source_path=self.local_backup_source_path,
                 sharepoint_destination_path=self.sharepoint_backup_base_url,
             )
